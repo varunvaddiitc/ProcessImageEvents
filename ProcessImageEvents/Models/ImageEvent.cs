8ebc94e6aca175c668a989c97547f1da7ae7cbf6
dv20aal <dv20aal@herts.ac.uk>
2024-04-08 12:48:29 +0100

    Added a new feature to process image events from blob container

---
+++ ProcessImageEvents/Models/ImageEvent.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ProcessImageEvents.Models
+{
+    public class ImageEvent
+    {
+        public string Id { get; set; }
+
+        public string Topic { get; set; }
+
+        public string Subject { get; set; }
+
+        public string EventType { get; set; }
+
+        public DateTime EventTime { get; set; }
+
+        public object Data { get; set; }
+    }
+}
